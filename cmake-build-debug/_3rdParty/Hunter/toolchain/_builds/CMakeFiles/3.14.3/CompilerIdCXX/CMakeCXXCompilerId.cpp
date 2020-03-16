28564b9df66847900424da88648bc1338c0f732f
@Maus_74 <nikita.potapovich@mail.ru>
2020-03-16 13:32:37 +0300

    end

--- cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/CMakeFiles/3.14.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
+++ cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/CMakeFiles/3.14.3/CompilerIdCXX/CMakeCXXCompilerId.cpp
@@ -1,6 +1,5 @@
-/* This source file must have a .cpp extension so that all C++ compilers
-   recognize the extension without flags.  Borland does not know .cxx for
-   example.  */
+// Copyright 2018 Your Name <your_email>
+
 #ifndef __cplusplus
 # error "A C compiler has been selected for C++."
 #endif
@@ -119,7 +118,8 @@
 # endif
 
 
-#elif defined(__ibmxl__) || (defined(__IBMCPP__) && !defined(__COMPILER_VER__) && __IBMCPP__ >= 800)
+#elif defined(__ibmxl__) || (defined(__IBMCPP__) &&
+!defined(__COMPILER_VER__) && __IBMCPP__ >= 800)
 # define COMPILER_ID "XL"
 # if defined(__ibmxl__)
 #  define COMPILER_VERSION_MAJOR DEC(__ibmxl_version__)
@@ -260,7 +260,8 @@
 #  define COMPILER_VERSION_TWEAK DEC(_MSC_BUILD)
 # endif
 
-#elif defined(__VISUALDSPVERSION__) || defined(__ADSPBLACKFIN__) || defined(__ADSPTS__) || defined(__ADSP21000__)
+#elif defined(__VISUALDSPVERSION__) || defined(__ADSPBLACKFIN__)
+|| defined(__ADSPTS__) || defined(__ADSP21000__)
 # define COMPILER_ID "ADSP"
 #if defined(__VISUALDSPVERSION__)
   /* __VISUALDSPVERSION__ = 0xVVRRPP00 */
@@ -356,7 +357,8 @@ char const *info_cray = "INFO" ":" "compiler_wrapper[CrayPrgEnv]";
 #elif defined(__sun) || defined(sun)
 # define PLATFORM_ID "SunOS"
 
-#elif defined(_AIX) || defined(__AIX) || defined(__AIX__) || defined(__aix) || defined(__aix__)
+#elif defined(_AIX) || defined(__AIX) || defined(__AIX__)
+|| defined(__aix) || defined(__aix__)
 # define PLATFORM_ID "AIX"
 
 #elif defined(__hpux) || defined(__hpux__)
@@ -539,7 +541,8 @@ char const *info_cray = "INFO" ":" "compiler_wrapper[CrayPrgEnv]";
 #ifdef COMPILER_VERSION_MAJOR
 char const info_version[] = {
   'I', 'N', 'F', 'O', ':',
-  'c','o','m','p','i','l','e','r','_','v','e','r','s','i','o','n','[',
+  'c', 'o', 'm', 'p', 'i', 'l', 'e', 'r', '_', 'v',
+  'e', 'r', 's', 'i', 'o', 'n', '[',
   COMPILER_VERSION_MAJOR,
 # ifdef COMPILER_VERSION_MINOR
   '.', COMPILER_VERSION_MINOR,
@@ -550,23 +553,25 @@ char const info_version[] = {
 #   endif
 #  endif
 # endif
-  ']','\0'};
+  ']', '\0'};
 #endif
 
 /* Construct a string literal encoding the internal version number. */
 #ifdef COMPILER_VERSION_INTERNAL
 char const info_version_internal[] = {
   'I', 'N', 'F', 'O', ':',
-  'c','o','m','p','i','l','e','r','_','v','e','r','s','i','o','n','_',
-  'i','n','t','e','r','n','a','l','[',
-  COMPILER_VERSION_INTERNAL,']','\0'};
+  'c', 'o', 'm', 'p', 'i', 'l', 'e', 'r', '_', 'v',
+  'e', 'r', 's', 'i', 'o', 'n', '_',
+  'i', 'n', 't', 'e', 'r', 'n', 'a', 'l', '[',
+  COMPILER_VERSION_INTERNAL, ']', '\0'};
 #endif
 
 /* Construct a string literal encoding the version number components. */
 #ifdef SIMULATE_VERSION_MAJOR
 char const info_simulate_version[] = {
   'I', 'N', 'F', 'O', ':',
-  's','i','m','u','l','a','t','e','_','v','e','r','s','i','o','n','[',
+  's', 'i', 'm', 'u', 'l', 'a', 't', 'e', '_', 'v',
+  'e', 'r', 's', 'i', 'o', 'n', '[',
   SIMULATE_VERSION_MAJOR,
 # ifdef SIMULATE_VERSION_MINOR
   '.', SIMULATE_VERSION_MINOR,
@@ -577,7 +582,7 @@ char const info_simulate_version[] = {
 #   endif
 #  endif
 # endif
-  ']','\0'};
+  ']', '\0'};
 #endif
 
 /* Construct the string literal in pieces to prevent the source from
